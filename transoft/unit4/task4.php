9e49c48909d45618e1b1be9e83f7707290c29658
Vlad Goyda <goyda2000@gmail.com>
2021-12-28 17:37:42 +0200

    Виправлення у 4 задачі 4 теми

--- transoft/unit4/task4.php
+++ transoft/unit4/task4.php
@@ -1,23 +1,27 @@
 <html>
 
 <head>
-    <title>Task 3.3</title>
+    <title>Task 4.4</title>
     <meta charset="UTF-8">
 </head>
 
 <body>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" autocomplete="off">
-        <textarea name="text" cols="30" rows="10"></textarea>
+        <textarea name="text" cols="30" rows="10" required></textarea>
+        <select name="language" required>
+            <option value="ukr">Українська</option>
+            <option value="eng">English</option>
+        </select>
         <input type="submit" value="Відправити">
     </form>
     <?php
     if (isset($_POST['text'])) {
-        $lang = 1; // 1 for eng, 2 for ukr
+        $lang = $_POST['language'] === "eng" ? 1 : 2;
         $text = $_POST['text'];
         $prevcheck = 0;
         $checkpoint = 0;
         for ($i = 0; $i < strlen($text); $i++) {
-            if($i == (40*$lang*($prevcheck+1))) $checkpoint++;
+            if ($i == (40 * $lang * ($prevcheck + 1))) $checkpoint++;
             if ($checkpoint > $prevcheck && $text[$i] === ' ') {
                 $text[$i] = "&";
                 $prevcheck = $checkpoint;
