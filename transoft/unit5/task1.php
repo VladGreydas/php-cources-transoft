a27d9c193683b8ee9975e2a80951c227892cfcea
Vlad Goyda <goyda2000@gmail.com>
2022-01-11 19:36:30 +0200

    Repair unit 5

--- transoft/unit5/task1.php
+++ transoft/unit5/task1.php
@@ -1,29 +1,28 @@
 <?php
-Function sort_uk( $a, $b ){
-    $a = mb_strtoupper ( $a, 'UTF-8' );
-    $b = mb_strtoupper ( $b, 'UTF-8' );
+function sort_uk($a, $b)
+{
+    $a = mb_strtoupper($a, 'UTF-8');
+    $b = mb_strtoupper($b, 'UTF-8');
     $alphabet = array(
-    'А' => 1, 'Б' => 2, 'В' => 3, 'Г' => 4, 'Д' => 5, 'Е' => 6, 'Є' => 7, 'Ж' => 8, 'З' => 9, 'И' => 10, 'І' => 11,
-    'Ї' => 12, 'Й' => 13, 'К' => 14, 'Л' => 15, 'М' => 16, 'Н' => 17, 'О' => 18, 'П' => 19, 'Р' => 20, 'С' => 21, 'Т' => 22,
-    'У' => 23, 'Ф' => 24, 'Х' => 25, 'Ц' => 26, 'Ч' => 27, 'Ш' => 28, 'Щ' => 29, 'Ь' => 30, 'Ю' => 31, 'Я' => 32
+        'А' => 1, 'Б' => 2, 'В' => 3, 'Г' => 4, 'Д' => 5, 'Е' => 6, 'Є' => 7, 'Ж' => 8, 'З' => 9, 'И' => 10, 'І' => 11,
+        'Ї' => 12, 'Й' => 13, 'К' => 14, 'Л' => 15, 'М' => 16, 'Н' => 17, 'О' => 18, 'П' => 19, 'Р' => 20, 'С' => 21, 'Т' => 22,
+        'У' => 23, 'Ф' => 24, 'Х' => 25, 'Ц' => 26, 'Ч' => 27, 'Ш' => 28, 'Щ' => 29, 'Ь' => 30, 'Ю' => 31, 'Я' => 32
     );
-    $lengthA = mb_strlen ( $a, 'UTF-8' );
-    $lengthB = mb_strlen ( $b, 'UTF-8' );
-    for( $i = 0; $i < ( $lengthA > $lengthB? $lengthB: $lengthA); $i++ ){
-        if ( $alphabet[ mb_substr( $a, $i, 1, 'UTF-8' ) ] < $alphabet[ mb_substr( $b, $i, 1, 'UTF-8' ) ] ){
+    $lengthA = mb_strlen($a, 'UTF-8');
+    $lengthB = mb_strlen($b, 'UTF-8');
+    for ($i = 0; $i < ($lengthA > $lengthB ? $lengthB : $lengthA); $i++) {
+        if ($alphabet[mb_substr($a, $i, 1, 'UTF-8')] < $alphabet[mb_substr($b, $i, 1, 'UTF-8')]) {
             $status = -1;
             break;
-        }
-        elseif ( $alphabet[ mb_substr( $a, $i, 1, 'UTF-8' ) ] > $alphabet[ mb_substr( $b, $i, 1, 'UTF-8' ) ] ){
+        } elseif ($alphabet[mb_substr($a, $i, 1, 'UTF-8')] > $alphabet[mb_substr($b, $i, 1, 'UTF-8')]) {
             $status = 1;
             break;
-        }
-        else{
+        } else {
             $status = 0;
         }
     }
     return $status;
-    }
+}
 
 function NameArrayProcessing(string $input): string
 {
--- transoft/unit5/task3.php
+++ transoft/unit5/task3.php
@@ -7,41 +7,78 @@
 
 <body>
     <?php
-    $sum = 0;
-    $oddCount = 0;
-    $even = array();
+    class MainValues
+    {
+        static $sum;
+        static $oddCount;
+        static $even;
+        static $abs;
 
-    $NumberProcess = function ($number) use (&$sum, &$oddCount, &$even) {
-        $sum += $number;
-        if ($number % 2 === 0) {
-            $oddCount++;
-        } else {
-            $even[] = $number;
+        public function __construct()
+        {
+            self::$sum = 0;
+            self::$oddCount = 0;
+            self::$even = null;
+            self::$abs = 0;
         }
-    };
+
+        public static function getValues(): array
+        {
+            return ["sum" => self::$sum, "odd" => self::$oddCount, "even" => self::$even, "abs" => self::$abs];
+        }
+
+        public static function SetABS(int $count)
+        {
+            self::$abs = self::$sum / $count;
+        }
+
+        public static function Reset()
+        {
+            self::$sum = null;
+            self::$oddCount = null;
+            self::$even = null;
+        }
+
+        public static function NumProcess(int $number)
+        {
+            self::$sum += $number;
+            if ($number % 2 === 0) {
+                self::$oddCount++;
+            } else {
+                self::$even[] = $number;
+            }
+        }
+    }
+
+    function StringToArrayTransformer(string $input): array
+    {
+        if (!empty($input)) {
+            $trimmedtext = trim($input);
+            $explodedText = explode(',', $trimmedtext);
+            for ($i = 0; $i < count($explodedText); $i++) {
+                $explodedText[$i] = trim($explodedText[$i]);
+            }
+            return $explodedText;
+        }
+    }
 
     if (isset($_POST['array'])) {
         $message = "";
-        $numArray = array();
-        $trimmedtext = trim($_POST['array']);
-        $explodedText = explode(',', $trimmedtext);
+        $explodedText = StringToArrayTransformer($_POST['array']);
         for ($i = 0; $i < count($explodedText); $i++) {
-            $explodedText[$i] = trim($explodedText[$i]);
-        }
-        for ($i = 0; $i < count($explodedText); $i++) {
-            if (preg_match('/\d+/', $explodedText[$i]) === 1) {
+            if (preg_match('/^[0-9]+$/', $explodedText[$i]) === 1) {
                 $number = (int) $explodedText[$i];
-                $NumberProcess($number);
+                MainValues::NumProcess($number);
             } else {
                 $message = "Array must contain only numbers";
-                $sum = null;
-                $oddCount = null;
-                $even = null;
+                MainValues::Reset();
                 break;
             }
         }
-        if (!is_null($sum))
-            $abs = $sum / count($explodedText);
+        if (!is_null(MainValues::$sum)) {
+            MainValues::SetABS(count($explodedText));
+            $valueArray = MainValues::getValues();
+        }
     }
     ?>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" autocomplete="off">
@@ -51,18 +88,18 @@
     <p style="color: #ff0000;"><? if (isset($message)) {
                                     echo $message;
                                 } ?></p>
-    <p>Сума: <? if (isset($sum) && !is_null($sum)) {
-            echo $sum;
-        } ?></p>
-    <p>Середнє значення: <? if (isset($abs) && !is_null($abs)) {
-            echo $abs;
-        } ?></p>
-    <p>Кількість парних чисел: <? if (isset($oddCount) && !is_null($oddCount)) {
-            echo $oddCount;
-        } ?></p>
-    <p>Непарні числа: <? if (isset($even) && !is_null($even)) {
-            echo implode(", ", $even);
-        } ?></p>
+    <p>Сума: <? if (isset($valueArray["sum"]) && !is_null($valueArray["sum"])) {
+                    echo $valueArray["sum"];
+                } ?></p>
+    <p>Середнє значення: <? if (isset($valueArray["abs"]) && !is_null($valueArray["abs"] && is_null($message))) {
+                                echo $valueArray["abs"];
+                            } ?></p>
+    <p>Кількість парних чисел: <? if (isset($valueArray["odd"]) && !is_null($valueArray["odd"] && is_null($message))) {
+                                    echo $valueArray["odd"];
+                                } ?></p>
+    <p>Непарні числа: <? if (isset($valueArray["even"]) && !is_null($valueArray["even"]) && is_null($message)) {
+                            echo implode(", ", $valueArray["even"]);
+                        } ?></p>
 </body>
 
 </html>
